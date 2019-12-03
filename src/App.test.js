f0797ddbc32d07f6bb7c5d7f28d5021eb799189c
Joe Wright <joe@joejag.com>
2019-12-03 15:21:40 +0000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -19,6 +19,9 @@ it('allows you to clear all of the todo items', () => {})
 it('can clear all items, add two new ones and delete the first', () => {
   // this one has a bug. We are using the test to reveal it
 })
+it('works with async calls', () => {
+  // Add a setTimeout(() => {},5000) within ToDo.createNewToDoItem()
+})
 it('show a message if all todo items are completed', () => {
   // this is a new feature. See if you can test drive it
 })
