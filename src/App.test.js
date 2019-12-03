9781844d2938a02f5d232f3c398fa9dfa3ef5461
Joe Wright <joseph.wright@ros.gov.uk>
2019-12-03 12:02:04 +0000

    Add testing library helper

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,12 @@
-it('has a test', () => {})
+import '@testing-library/jest-dom/extend-expect'
+
+import { fireEvent, render } from '@testing-library/react'
+
+import App from './App'
+import React from 'react'
+import expectExport from 'expect'
+
+it('has a sample test', () => {
+  const { getByText } = render(<App />)
+  expectExport(getByText('React To Do')).toBeInTheDocument()
+})
