01435513a88f94dce18882903cceb23a28e35ea3
Joe Wright <joseph.wright@ros.gov.uk>
2020-01-13 14:43:14 +0000

    fix(expect): fix the expect import

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,11 @@ import '@testing-library/jest-dom/extend-expect'
 
 import App from './App'
 import React from 'react'
-import expectExport from 'expect'
 import { render } from '@testing-library/react'
 
 it('has a sample test', () => {
   const { getByText } = render(<App />)
-  expectExport(getByText('React To Do')).toBeInTheDocument()
+  expect(getByText('React To Do')).toBeInTheDocument()
 })
 
 // TODO: Complete these tests
